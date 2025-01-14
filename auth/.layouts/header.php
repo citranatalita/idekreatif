cc11fd90c7e6f50f8a8ff0966913b17491b8194b
citranatalita <citranatalitapurba@gmail.com>
2025-01-14 12:27:02 +0700

    menambahkan header auth

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -1,3 +1,26 @@
+<?php
+session_start();
+
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION['notification']);
+}
+
+?>
+
+
+<?php
+
+if (isset($_SESSION["username"]) || isset($_SESSION["role"])) {
+  $_SESSION['notification'] = [
+    'type' => 'danger',
+    'message' => 'Silakan Logout Terlebih Dahulu!'
+  ];
+  header('location: ../dashboard.php');
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
