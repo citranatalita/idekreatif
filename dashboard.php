534338b3a730baa1294715fcd7299e232c20a4dc
citranatalita <citranatalitapurba@gmail.com>
2025-02-04 13:28:44 +0700

    menambahkan bagian5 dan 6

--- dashboard.php
+++ dashboard.php
@@ -60,4 +60,55 @@ include '.includes/toast_notification.php';
                                                 <a href="edit_post.php?post_id=<?= $post['id_post']; ?>" class="dropdown-item">
                                                     <i class="bx bx-edit-alt me-2"></i> Edit
                                                 </a>
-                                               
+
+                                               <!-- Pilihan Delete -->
+                                                <a href="#" class="dropdown-item" data-bs-toggle="modal"
+                                                data-bs-target="#deletePost_<?= $post['id_post']; ?>">
+                                                <i class="bx bx-trash me-2"></i> Delete
+                                                </a>
+                                            </div>
+                                            </div>
+                                                </td>
+                                                </tr>
+                                                <!-- Modal untuk Hapus Konten Blog -->
+                                            <div class="modal fade" id="deletePost_<?= $post['id_post']; ?>"
+                                                tabindex="-1" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Hapus Post?</h5>
+                                                <button type="button" class="btn-close" data-bs-
+                                                dismiss="modal"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="proses_post.php" method="POST">
+                                            <div>
+                                                <p>Tindakan ini tidak bisa dibatalkan.</p>
+                                                <input type="hidden" name="postID" value="<?=
+                                                $post['id_post']; ?>">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-outline-secondary"
+                                                data-bs-dismiss="modal">Batal</button>
+                                                <button type="submit" name="delete" class="btn btn-
+                                                primary">Hapus</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </tbody>
+                </table>
+                </div>
+                </div>
+                </div>
+                <!-- Akhir tabel dengan baris yang dapat di-hover -->
+                </div>
+            </div>
+
+
+            <?php
+            include (".includes/footer.php");
+            ?>
